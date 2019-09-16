6a0b684d5350290ac8b9677910be6112734bc740
Ian Everett <ian_everett@me.com>
2019-09-16 06:07:23 +0100

    Add some array functions. avg etc..

--- numpy/main.py
+++ numpy/main.py
@@ -6,7 +6,6 @@ def redraw(board):
         print(row)
 
 current_board = np.zeros((3, 3))    # Create an 3 x 3 array of all zeros
-redraw(current_board)
 
 # fill some data
 current_board[0][0] = 1
@@ -25,3 +24,13 @@ print('row %s' % row)
 print('Are all numbers in row set to 1? % r' % np.all(row == 1))
 
 redraw(current_board)
+
+for i in range(5):
+    print('')
+
+b = np.array([[100, 23, 10, 10], [102, 26, 20, 10]])
+print(b.shape)
+
+print('avg = %s' % np.average(b, 0))
+print('max = %s' % np.max(b, 0))
+print('min = %s' % np.min(b, 0))
