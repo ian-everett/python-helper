f2caf37609e571de91ffb5c78a2508a639d04094
Ian Everett <ian_everett@me.com>
2019-09-24 19:06:41 +0100

    Add more Qt Pyside2

---
+++ Qt-pyside2-qml/main.py
@@ -0,0 +1,12 @@
+from PySide2.QtWidgets import QApplication
+from PySide2.QtQuick import QQuickView
+from PySide2.QtCore import QUrl
+
+
+app = QApplication([])
+
+view = QQuickView()
+view.setSource(QUrl("view.qml"))
+view.show()
+
+app.exec_()
---
+++ Qt-pyside2-qml/view.qml
@@ -0,0 +1,14 @@
+import QtQuick 2.0
+
+Rectangle {
+    id: rootId
+
+    width: 600
+    height: 400
+    color: 'dodgerblue'
+
+    Text {
+        text: "Hello"
+        anchors.centerIn: parent
+    }
+}
