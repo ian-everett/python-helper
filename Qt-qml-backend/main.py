3e9bc01fff8a7b9cb9db8bcc2c3628a6ef9bbfd0
Ian Everett <ian_everett@me.com>
2019-09-26 13:35:15 +0100

    Add an example of a backend.

--- Qt-qml-backend/main.py
+++ Qt-qml-backend/main.py
@@ -1,10 +1,13 @@
 """
 Example of Qt PySide2 with a Python backend and qml frontend
+typically this use case is for when the front end needs to
+do file I/O rather than below.
 """
 import sys
 from PySide2.QtCore import QObject
 from PySide2.QtCore import Property
 from PySide2.QtCore import Signal
+from PySide2.QtCore import QTimer
 from PySide2.QtCore import QUrl
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import qmlRegisterType
@@ -17,22 +20,46 @@ class Backend(QObject):
     """
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.__value = 42
+        self._value = 42
 
-    def _value(self):
-        """
-        Get the value
-        """
-        return self.__value
+        # setup a timer to fire every second
+        # pylint: disable=E1101
+        timer = QTimer(self)
+        timer.start(1000)
+        timer.timeout.connect(self.update)
 
+    #
+    # update
+    #
+    def update(self):
+        """
+        inc value counter every second
+        """
+        self.set_value(self._value + 1)
+
+    #
+    # value signal & getter/setter
+    #
     @Signal
     def value_changed(self):
-        """
-        value changed signal
-        """
+        """value changed signal"""
 
-    value = Property(int, _value, notify=value_changed)
+    @Property(int, notify=value_changed)
+    def value(self):
+        """
+        value getter
+        """
+        return self._value
 
+    @value.setter
+    def set_value(self, value):
+        """
+        value setter
+        """
+        if value != self._value:
+            self._value = value
+            # pylint: disable=E1101
+            self.value_changed.emit()
 
 
 def main():
