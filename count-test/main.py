b629b23e98595b9e8f23a91d8b115916afc451b0
Ian Everett <ian_everett@me.com>
2019-10-10 12:52:52 +0100

    Python uses capitals (like most others) for constants.

--- count-test/main.py
+++ count-test/main.py
@@ -1,18 +1,13 @@
 
 # Find out correct way of using constants in Python
-max_zones = 128
-zones_per_module = 12
-zones_per_block = 4
+MAX_ZONES = 64
+USB_ZONES_PER_BLK = 16
 
-for i in range(max_zones):
+
+for i in range(MAX_ZONES):
     # Print zone
     print("zone %d" % i, end='\t')
 
-    # Get module number
-    module = (i / zones_per_module)
-    print(" module %d" % module, end='\t')
-
     # Get block number
-    block = (i % zones_per_module) / zones_per_block
-    print("  block %d" % block, end='\n')
-
+    block = i / USB_ZONES_PER_BLK
+    print("block %d" % block, end='\n')
