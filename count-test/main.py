d768c965c4ffb32130c842422bdfdff3269139d2
Ian Everett <ian_everett@me.com>
2019-10-11 16:35:42 +0100

    Add an enum example

--- count-test/main.py
+++ count-test/main.py
@@ -1,8 +1,11 @@
+'''
+How to use constants and enums
+'''
+from enum import Enum, unique
 
-# Find out correct way of using constants in Python
+# Set some constants
 MAX_ZONES = 64
 USB_ZONES_PER_BLK = 16
-
 
 for i in range(MAX_ZONES):
     # Print zone
@@ -11,3 +14,28 @@ for i in range(MAX_ZONES):
     # Get block number
     block = i / USB_ZONES_PER_BLK
     print("block %d" % block, end='\n')
+
+
+# Setup a enum with unique numbers
+@unique
+class NetworkSetting(Enum):
+    '''
+    NetworkSettings constants
+    '''
+    IP_ADDRESS = 0
+    NETMASK = 1
+
+
+def set_settings():
+    '''
+    set_settings
+    '''
+    # There seems no gain doing things like this
+    settings = {}
+    settings[NetworkSetting.IP_ADDRESS] = '192.168.1.1'
+    settings[NetworkSetting.NETMASK] = '255.255.255.1'
+
+    # When this can be done
+    settings2 = {}
+    settings2['IP_ADDRESS'] = '192.168.1.1'
+    settings2['NETMASK'] = '255.255.255.1'
