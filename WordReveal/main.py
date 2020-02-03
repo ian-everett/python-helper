88a98d945526a7bad642504f25960ebe8d8af696
Ian Everett <ian_everett@me.com>
2020-02-03 09:30:35 +0000

    Add a game test to see if word has been matched and break up into smaller functions.

--- WordReveal/main.py
+++ WordReveal/main.py
@@ -19,32 +19,57 @@ def get_random_word(words):
 
     return ''
 
-def print_current_state(word, current):
+
+def print_current_guess(word, current):
     '''
-    print_current_state
+    Outputs the current word guess
     '''
     print("Guess Word: ", end='')
     for letter in word:
         if letter.lower() in current:
             print(letter, end='')
+        elif letter == ' ':
+            print(' ', end='')
         else:
             print('%c' % '-', end='')
     print('')
 
+
+def show_drawn_letters(current):
+    '''
+    Ouputs the currently drawn letters
+    '''
+    # Display all the letters which have been
+    # drawn in alphabetical order
+    for item in sorted(current):
+        print("%c " % item, end='')
+
+
+def get_current_state(word, current):
+    '''
+    Outputs the current game state and returns
+    wether the word has been gussed with True or
+    False if not yet.
+    '''
+    print_current_guess(word, current)
+    print('')
+
     # Show all letters that have been picked thus far
     if current != set():
-        # Display in alphabetical order
-        for item in sorted(current):
-            print("%c " % item, end='')
-        print('')
+        show_drawn_letters(current)
         print('')
 
-        # Check to see if we have matched all letters
+        # Check to see if we have matched all letters if
+        # so we we return True to stop cureent game.
+        if set(word.lower()).issubset(current):
+            return True
+
+    return False
 
 
 def main():
     '''
-    main()
+    main entry point
     '''
 
     # Open given file for reading, list of words should
@@ -70,7 +95,11 @@ def main():
 
             # Add the next letter to the chosen set
             chosen.add(letter)
-            print_current_state(word, chosen)
+
+            # Show current state of game and if we have a match quit loop
+            if get_current_state(word, chosen):
+                print('Got a match took %d goes.' % (n_goes + 1))
+                break
 
             time.sleep(2.0)
 
